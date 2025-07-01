4938a3d7af2c8f98b33fe53e947175f1f7fe91cf
Vishnu c <vishnumalbro124@gmail.com>
2025-07-01 22:19:59 +0530

    poster grid section animation completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,28 +17,28 @@ const Navbar = () => {
     const [appsHover, setAppsHover] = useState(false)
     const [EnHover, setEnHover] = useState(false)
     return (
-        <nav className='w-full'>
+        <nav className='w-full bg-black'>
             <div className="text-white flex justify-start items-center pl-5 ">
                 <ul className='flex justify-start gap-2 space-x-4 ml-5 px-5 p-4'>
-                    <li className='text-[14px] p-1 '>prime video</li>
-                    <li className='text-[10px] font-semibold p-2 bg-black hover:text-black hover:bg-white hover:rounded-md transition-200 duration-300 w-15 justify-center flex'>Home</li>
-                    <li className='text-[10px] font-semibold p-2 bg-black hover:text-black hover:bg-white hover:rounded-md transition-200 duration-300 w-15 justify-center flex '>Movies</li>
-                    <li className='text-[10px] font-semibold p-2 bg-black hover:text-black hover:bg-white hover:rounded-md transition-200 duration-300 w-15 justify-center flex '>TV shows</li>
-                    <li className='text-[10px] font-semibold p-2 bg-black hover:text-black hover:bg-white hover:rounded-md transition-200 duration-300 w-15 justify-center flex '>Live TV</li>
-                    <li className='text-[10px] font-semibold p-2 '>|</li>
+                    <li className='text-[18px] p-1 '>prime video</li>
+                    <li className='text-[16px] font-semibold p-1 bg-black hover:text-black hover:bg-white hover:rounded-md transition-200 duration-300 w-20 justify-center flex'>Home</li>
+                    <li className='text-[16px] font-semibold p-1 bg-black hover:text-black hover:bg-white hover:rounded-md transition-200 duration-300 w-20 justify-center flex '>Movies</li>
+                    <li className='text-[16px] font-semibold p-1 bg-black hover:text-black hover:bg-white hover:rounded-md transition-200 duration-300 w-20 justify-center flex '>TV shows</li>
+                    <li className='text-[16px] font-semibold p-1 bg-black hover:text-black hover:bg-white hover:rounded-md transition-200 duration-300 w-20 justify-center flex '>Live TV</li>
+                    <li className='text-[16px] font-semibold p-1 '>|</li>
                     <li onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} className="flex items-center space-x-3 p-2 font-semibold bg-black hover:text-black hover:bg-white hover:rounded-md transition-200 duration-300 justify-center ">
                         {/* <img className="w-4 h-4" src={Icon} alt="" /> */}
-                        {isHovered ? <img className="w-4 h-4" src={BlackIcon} alt="" /> : <img className="w-4 h-4" src={Icon} alt="" />}
-                        <span className='text-[10px]'>Subscription</span>
+                        {isHovered ? <img className="w-6 h-6" src={BlackIcon} alt="" /> : <img className="w-6 h-6" src={Icon} alt="" />}
+                        <span className='text-[16px]'>Subscription</span>
                     </li>
                 </ul>
-                <div className='ml-5 w-3xl h-20 p-3'>
+                <div className='ml-5 w-2xl h-20 p-3'>
                     <ul className='flex justify-end gap-2 space-x-4 ml-5 px-5 p-4'>
-                        <li onMouseEnter={() => setSearchHover(true)} onMouseLeave={() => setSearchHover(false)} className="flex items-center space-x-3 p-2 font-semibold bg-black hover:text-black hover:bg-white hover:rounded-md transition-200 duration-300 justify-center ">{searchHover ? <img className="w-4 h-4" src={blackSearch} alt="" /> : <img className="w-4 h-4" src={WhiteSearch} alt="" />}</li>
-                        <li onMouseEnter={() => setEnHover(true)} onMouseLeave={() => setEnHover(false)} className="flex items-center space-x-3 p-2 font-semibold bg-black hover:text-black hover:bg-white hover:rounded-md transition-200 duration-300 justify-center ">{EnHover ? <img className="w-4 h-4" src={blacken} alt="" /> : <img className="w-4 h-4" src={whiteEn} alt="" />}</li>
-                        <li onMouseEnter={() => setAppsHover(true)} onMouseLeave={() => setAppsHover(false)} className="flex items-center space-x-3 p-2 font-semibold bg-black hover:text-black hover:bg-white hover:rounded-md transition-200 duration-300 justify-center ">{appsHover ? <img className="w-4 h-4" src={blackDot} alt="" /> : <img className="w-4 h-4" src={whiteDot} alt="" />}</li>
-                        <li  className="flex items-center space-x-3 font-semibold hover:border-2 w-9 rounded-full  justify-center "><img className="w-4 h-4" src={userLogo} alt="" /></li>
-                        <li className='text-[10px] font-bold p-2 bg-blue-500 rounded-md hover:bg-blue-400 '>Join prime</li>
+                        <li onMouseEnter={() => setSearchHover(true)} onMouseLeave={() => setSearchHover(false)} className="flex items-center space-x-3 p-4 font-semibold bg-black hover:text-black hover:bg-white hover:rounded-md transition-200 duration-300 justify-center ">{searchHover ? <img className="w-6 h-6" src={blackSearch} alt="" /> : <img className="w-6 h-6" src={WhiteSearch} alt="" />}</li>
+                        <li onMouseEnter={() => setEnHover(true)} onMouseLeave={() => setEnHover(false)} className="flex items-center space-x-3 p-4 font-semibold bg-black hover:text-black hover:bg-white hover:rounded-md transition-200 duration-300 justify-center ">{EnHover ? <img className="w-6 h-6" src={blacken} alt="" /> : <img className="w-6 h-6" src={whiteEn} alt="" />}</li>
+                        <li onMouseEnter={() => setAppsHover(true)} onMouseLeave={() => setAppsHover(false)} className="flex items-center space-x-3 p-4 font-semibold bg-black hover:text-black hover:bg-white hover:ro unded-md transition-200 duration-300 justify-center ">{appsHover ? <img className="w-6 h-6" src={blackDot} alt="" /> : <img className="w-6 h-6" src={whiteDot} alt="" />}</li>
+                        <li  className="flex items-center space-x-3 font-semibold hover:border-4 w-9 h-10 mt-2 rounded-full  justify-center "><img className="w-6 h-6" src={userLogo} alt="" /></li>
+                        <li className='text-[16px] font-bold bg-blue-500 rounded-md hover:bg-blue-400 h-10 mt-2 w-24 flex justify-center items-center'>Join Prime</li>
                     </ul>
                 </div>
             </div>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,18 +1,37 @@
 
 import React from 'react'
 import Navbar from '../components/Navbar'
+import poster1 from '/login/poster1.jpg'
+import poster2 from '/login/poster2.jpg'
+import poster3 from '/login/poster3.jpg'
+import poster4 from '/login/poster4.jpg'
+import centerPoster from '/login/centerposter.jpg'
+import {motion} from 'framer-motion'
 
 const Login = () => {
-  return (
-    <div>
-      <Navbar/>
-      {/* hero section */}
-      <div className='flex justify-center items-center'>
-        <div className='w-50 h-screen bg-red-300'></div>
-        <div className='w-50 h-screen bg-yellow-200'></div>
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <Navbar />
+            {/* hero section */}
+            <div className='flex justify-center items-center'>
+                <div className='w-screen h-[80vh] flex justify-center items-center'>
+                    <h1 className='text-white text-6xl font-semibold font-serif mb-10 ml-5 pl-5'>Discover, Search, Repeat.</h1>
+                </div>
+                <div className='absolute w-[10vw] h-[85vh] bg-black blur-2xl  ml-20  z-9'></div>
+                <div className='w-screen h-[80vh]'>
+                    <motion.div initial={{opacity:0,rotate:180, x:520}} animate={{rotate:0 ,opacity:1,x:0}} transition={{duration:2,delay:0.5}} className="gap-1 p-6 w-full h-[80vh] grid grid-cols-2 grid-rows-2 relative">
+                        <div  className=" z-1  w-full h-full bg-black bg-opacity-50"> <img className='rounded-full w-full h-full object-cover' src={poster1} alt="" /></div>
+                        <div className="w-full h-full"><img className='rounded-full w-full h-full object-cover' src={poster2} alt="" /></div>
+                        <div className="w-full h-full"><img className='rounded-full w-full h-full object-cover' src={poster3} alt="" /></div>
+                        <div className="w-full h-full"><img className='rounded-full w-full h-full object-cover mb-10' src={poster4} alt="" /></div>
+                        <motion.div initial={{scale:0,opacity:0}} animate={{opacity:1,scale:1.2}} transition={{duration:5 , delay:3}} className="absolute w-80 h-80 z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center "><img className='rounded-md' src={centerPoster} alt="" /></motion.div>
+
+                    </motion.div>
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Login
